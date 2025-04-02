e6c9509b19f1cfffefbd40c1d7b33108fabcc4c2
Anastasiahack <anastashack789@gmail.com>
2025-04-02 05:16:33 -0700

    second commit

--- day01/homework/homework1.py
+++ day01/homework/homework1.py
@@ -1,2 +1,32 @@
-print("Anastasia")
-print("this is day 1")
+_name="Anastasias"
+#name არის ცვლადი
+#=არის ცვლადისთვის მნიშვნელოვანი მინიჭებული სიმბოლო
+#"Anastasia" არის ცვლადისტვის მნიშვნელობა
+#პრინტ ფუნქციაას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+#print(name)
+
+
+knows_programming=3
+surneme="Anastasia"
+name="Anastasia"# ეს არის str (string) ტიპის ცვლადი
+age=88 # ეს არის int (integer) მთელი რიცხვი
+height=184.5 # ეს არის float ტიპის ცვლადი (ათწილადი)
+#bolean (bool) ტიპის ცვლადი
+
+
+#knows_programming=true 3true ან false is_uglu=False # ჯავასკრიპტული comelcasess
+#print(name+""+ surname)
+## სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+print(type(age))
+print(type(name))
+print(type(surneme))
+print(type(height))
+print(type(knows_programming))
+
+
+
+print(name + " " + str(age))
+
+
