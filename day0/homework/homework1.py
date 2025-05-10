36da28574e585d6fc5b0a3b5d2304172755a92b2
Anastasiahack <anastashack789@gmail.com>
2025-05-10 15:02:07 -0700

    Third commit

--- day0/homework/homework1.py
+++ day0/homework/homework1.py
@@ -1,32 +1,33 @@
-_name="Anastasias"
-#name არის ცვლადი
+fruit="pineapple"
+#pineapple არის ცვლადი
 #=არის ცვლადისთვის მნიშვნელოვანი მინიჭებული სიმბოლო
-#"Anastasia" არის ცვლადისტვის მნიშვნელობა
+#"pineapple" არის ცვლადისთვის მნიშვნელობა
 #პრინტ ფუნქციაას გადაეცემა ეკრანზე გამოსატანი ობიექტი
 
-#print(name)
+#print(fruit)
 
 
 knows_programming=3
-surneme="Anastasia"
-name="Anastasia"# ეს არის str (string) ტიპის ცვლადი
+surneme="pineapple"
+name="pineapple"# ეს არის str (string) ტიპის ცვლადი
 age=88 # ეს არის int (integer) მთელი რიცხვი
 height=184.5 # ეს არის float ტიპის ცვლადი (ათწილადი)
 #bolean (bool) ტიპის ცვლადი
 
 
 #knows_programming=true 3true ან false is_uglu=False # ჯავასკრიპტული comelcasess
-#print(name+""+ surname)
+#print(fruit+""+ surname)
 ## სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
 
 print(type(age))
-print(type(name))
+print(type(fruit))
 print(type(surneme))
 print(type(height))
 print(type(knows_programming))
 
 
 
-print(name + " " + str(age))
+print(fruit + " " + str(age))
+
 
 
