1575e1aa0efa02ae7f09f0fef9c28dfd6bf5e828
Anastasiahack <anastashack789@gmail.com>
2025-05-11 09:49:24 -0700

    second commit

---
+++ day02/Homework/homework02.py
@@ -0,0 +1,10 @@
+ # 1. ორი ცვლადის შექმნა მათემატიკური ოპერაციები
+
+number1=897
+number2=7
+
+print(number1 +  number2)
+print(number1 -  number2)
+print(number1 *  number2)
+print(number1 /  number2)
+print(number1 // number2)
