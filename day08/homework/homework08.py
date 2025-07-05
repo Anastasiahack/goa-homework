f6967a09e47c5e1c0fa7f90b5f8ec8216e8b56d8
Anastasiahack <anastashack789@gmail.com>
2025-07-05 04:41:44 -0700

    Eighth commit

---
+++ day08/homework/homework08.py
@@ -0,0 +1,30 @@
+# დავალება: 1 
+
+#string-ბრჭყალებში მოქცეული სიმბოლოები
+#integer-ყველა მთელი რიცხვი მინუს უსასრულობიდან პლიუს უსასრულობამდე
+#float-ყველა მთელი რიცხვი
+#boolean-ლოგიკური ტიპის მნიშვნელობა, რომელსაც აქვს ორი მნიშვნელობა, True-კი და False-არა.
+
+
+#დავალება: 2
+
+Fruit="Coconut"
+
+Raodia=100
+
+height=29.100
+
+is_proggrammer=True
+
+is_gamer=False
+
+
+print( type (Fruit) )
+
+print( type (Raodia) )
+
+print( type (height) )
+
+print( type (is_proggrammer) )
+
+print( type (is_gamer) )
